8c655fc36e054b8c639ae3166e44927c294069e7
Shaun Wilesmith <shaun.wilesmith@gmail.com>
2018-02-19 21:01:34 +1000

    Reset version number.

--- CustomTimeTrials/Properties/AssemblyInfo.cs
+++ CustomTimeTrials/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CustomTimeTrials")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds Custom Time Trials to GTA V")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ShaunRW")]
 [assembly: AssemblyProduct("CustomTimeTrials")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -24,13 +24,8 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//      Major Version: New stand alone version. Breaks compatiblity with previous versions. Complete overhaul of the internal systems.
+//      Minor Version: New features / bug fixes. Will be compatible with datafiles generated from within this major version.
+//      bugfix Version: No new features. Just non intrusive bug fixes.
+
+[assembly: AssemblyVersion("0.0.0.*")]
